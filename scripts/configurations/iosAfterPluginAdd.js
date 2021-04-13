b7bb3977d0f69c344262c1ab05397fa853a5511b
Craig St. Jean <craig@frozenrosesoftware.com>
2021-04-12 22:37:20 -0400

    Adjust search paths

--- scripts/configurations/iosAfterPluginAdd.js
+++ scripts/configurations/iosAfterPluginAdd.js
@@ -92,15 +92,22 @@ module.exports = function(context) {
 
   var pbxFileCtor = Object.getPrototypeOf(fakeFile).constructor;
 
-  var addCopyFile = function(path, fileRef) {
+  var addCopyFile = function(path, target, fileRef) {
     var bundleFile = construct(pbxFileCtor, [path]);
     bundleFile.uuid = myProj.generateUuid();
+
     if (typeof(fileRef) === 'undefined') {
       bundleFile.fileRef = myProj.generateUuid();
     } else {
       bundleFile.fileRef = fileRef;
     }
-    bundleFile.target = myProj.getFirstTarget().uuid;
+
+    if (typeof(target) === 'undefined' || target == null) {
+      bundleFile.target = myProj.getFirstTarget().uuid;
+    } else {
+      bundleFile.target = target;
+    }
+    
     myProj.addToPbxBuildFileSection(bundleFile);
     myProj.addToPbxFileReferenceSection(bundleFile);
     myProj.addToPbxCopyfilesBuildPhase(bundleFile);
@@ -108,15 +115,22 @@ module.exports = function(context) {
     return bundleFile;
   };
 
-  var addResourceFile = function(path, fileRef) {
+  var addResourceFile = function(path, target, fileRef) {
     var bundleFileResource = construct(pbxFileCtor, [path]);
     bundleFileResource.uuid = myProj.generateUuid();
+
     if (typeof(fileRef) === 'undefined') {
       bundleFileResource.fileRef = myProj.generateUuid();
     } else {
       bundleFileResource.fileRef = fileRef;
     }
-    bundleFileResource.target = myProj.getFirstTarget().uuid;
+
+    if (typeof(target) === 'undefined' || target == null) {
+      bundleFileResource.target = myProj.getFirstTarget().uuid;
+    } else {
+      bundleFileResource.target = target;
+    }
+
     myProj.addToPbxBuildFileSection(bundleFileResource);
     myProj.addToPbxResourcesBuildPhase(bundleFileResource);
     
@@ -131,7 +145,12 @@ module.exports = function(context) {
   myProj.addToPbxFileReferenceSection(xcframeworkFile);
   myProj.addToFrameworksPbxGroup(xcframeworkFile);
   myProj.addToPbxFrameworksBuildPhase(xcframeworkFile);
-  myProj.addToFrameworkSearchPaths(xcframeworkFile);
+
+  var searchFile = construct(pbxFileCtor, ['LPMessagingSDK/LPMessagingSDK.xcframework']);
+  searchFile.uuid = myProj.generateUuid();
+  searchFile.fileRef = xcframeworkFile.fileRef;
+  searchFile.target = myProj.getFirstTarget().uuid;
+  myProj.addToFrameworkSearchPaths(searchFile);
 
   var embedFile = construct(pbxFileCtor, ['LPMessagingSDK/LPMessagingSDK.xcframework']);
   embedFile.uuid = myProj.generateUuid();
@@ -143,11 +162,12 @@ module.exports = function(context) {
   console.log('Adding LPMessagingSDKModels.bundle to Resources');
   myProj.addBuildPhase([], 'PBXCopyFilesBuildPhase', 'Copy Files', myProj.getFirstTarget().uuid);
   var file = addCopyFile('LPMessagingSDK/LPMessagingSDKModels.bundle');
-  addResourceFile('LPMessagingSDK/LPMessagingSDKModels.bundle', file.fileRef);
+  addResourceFile('LPMessagingSDK/LPMessagingSDKModels.bundle', null, file.fileRef);
 
   console.log('Adding LPMessagingSDK.xcframework to Resources');
-  file = addCopyFile('LPMessagingSDK/LPMessagingSDK.xcframework');
-  addResourceFile('LPMessagingSDK/LPMessagingSDK.xcframework', file.fileRef);
+  var pluginsGroup = myProj.pbxGroupByName('Frameworks');
+  file = addCopyFile('LPMessagingSDK/LPMessagingSDK.xcframework', frameworksUuid);
+  addResourceFile('LPMessagingSDK/LPMessagingSDK.xcframework', frameworksUuid, file.fileRef);
   
   var configurations = nonComments(myProj.pbxXCBuildConfigurationSection());
   for (var config in configurations) {
